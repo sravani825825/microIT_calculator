2473d8a80938ea261dbbb11180a28ea96e73aed4
sravani825825 <surasravani56@gmail.com>
2025-06-02 21:11:23 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,20 @@
+
+const display = document.getElementById('display');
+
+function append(value) {
+    document.getElementById('display').value += value;
+}
+function clearDisplay() {
+    display.value = '';
+}
+function backspace() {
+    display.value = display.value.slice(0,-1);
+}
+function calculateResult() {
+    try{
+        display.value = eval(display.value);
+    }
+    catch {
+        display.value='Error';
+    }
+}
